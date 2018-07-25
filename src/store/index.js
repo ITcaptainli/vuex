39666b4e8bea76a1308d73338ba26ed45443bd81
ending <ending.li@ximalaya.com>
2018-07-25 18:30:14 +0800

    修改

--- src/store/index.js
+++ src/store/index.js
@@ -3,12 +3,12 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
-let homeListNameStr = ''
-let homeListImgSrcStr = ''
+let homeListNameStr = 'Lee'
+let homeListImgSrcStr = 'http://demo.zmblogs.com/vue/vuexDemo/images/lee.jpg'
 try {
   if (localStorage.homeListName) {
-    homeListNameStr = localStorage.homeListName || 'Lee'
-    homeListImgSrcStr = localStorage.homeListImgSrc || 'http://demo.zmblogs.com/vue/vuexDemo/images/lee.jpg'
+    homeListNameStr = localStorage.homeListName
+    homeListImgSrcStr = localStorage.homeListImgSrc
   }
 } catch (e) {}
 
